a9cf619144f088f1a42529fca2587985dbeb3266
jake <tyrannizerdev@gmail.com>
2022-04-20 18:49:40 -0700

    update

--- server/devtools.lua
+++ server/devtools.lua
@@ -1,6 +1,4 @@
 easyCore.Dev = {}
-local inspect = exports["easyCore"]:GetModule("inspect")
-
 easyCore.Dev.Error = function(msg)
     print('[easyCore:ERROR] ' .. msg)
 end
@@ -9,8 +7,31 @@ easyCore.Dev.Log = function(msg)
     print('[easyCore:LOG] ' .. msg)
 end
 
-easyCore.Dev.Dump = function(table)
-    print(inspect(table))
+easyCore.Dev.Dump = function(table, indent)
+    if not indent then
+        indent = 0
+    end
+    local toprint = string.rep(" ", indent) .. "{\r\n"
+    indent = indent + 2
+    for k, v in pairs(tbl) do
+        toprint = toprint .. string.rep(" ", indent)
+        if (type(k) == "number") then
+            toprint = toprint .. "[" .. k .. "] = "
+        elseif (type(k) == "string") then
+            toprint = toprint .. k .. "= "
+        end
+        if (type(v) == "number") then
+            toprint = toprint .. v .. ",\r\n"
+        elseif (type(v) == "string") then
+            toprint = toprint .. "\"" .. v .. "\",\r\n"
+        elseif (type(v) == "table") then
+            toprint = toprint .. easyCore.Dev.Dump(v, indent + 2) .. ",\r\n"
+        else
+            toprint = toprint .. "\"" .. tostring(v) .. "\",\r\n"
+        end
+    end
+    toprint = toprint .. string.rep(" ", indent - 2) .. "}"
+    print(toprint)
 end
 
 -- to add. https://forum.cfx.re/t/release-warmenu-lua-menu-framework/41249
